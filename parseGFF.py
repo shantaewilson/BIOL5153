f04c5d5d10e008068ad54860c8e03b1cdf1bc4b7
Quincy Ward <qkward@uaf134984.ddns.uark.edu>
2018-04-11 09:35:29 -0500

    now use argparse

--- parseGFF.py
+++ parseGFF.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
+# load the required modules
+import argparse
 from Bio import SeqIO
+
+
+#create an ArgumentParser object ('parser') that will hold the information necessary to parse the command line
+parser = argparse.ArgumentParser(description="This script accepts inputs from GFF and FASTA file")
+
+# use add_argument() method to add a positional argument
+#positional arguments are *required* inputs, so their order/position matters
+#argparse treats all options as strings unless told to do otherwise
+parser.add_argument("fasta",help="name of FASTA file")
+parser.add_argument("gff",help="name of GFF file")
+
+
+#parse the arguments
+args = parser.parse_args()
+
+
+print("We're gonna open this FASTA file:", args.fasta)
+print("We're gonna open this GFF file:", args.gff)
+
 
 # read in genome and store in a variable
 for genome in SeqIO.parse("/users/qkward/Desktop/watermelon_files/watermelon.fsa", "fasta"):
@@ -12,15 +33,14 @@ for genome in SeqIO.parse("/users/qkward/Desktop/watermelon_files/watermelon.fsa
 GFF_file = "/users/qkward/Desktop/watermelon_files/watermelon.gff"
 GFF_in = open(GFF_file, 'r')
 
-import sys
 for line in GFF_in:
 # split string into list
-        coordinates = line.split("\t")
-        start = int(coordinates[3])
-        end = int(coordinates[4])
-        org_name = (coordinates[0])
+        fields = line.split("\t")
+        start = int(fields[3])
+        end = int(fields[4])
+        org_name = (fields[0])
        # print(org_name)
-        gene_name = (coordinates[8])
+        gene_name = (fields[8])
        # print(gene_name)
         print(org_name + gene_name + genome[start:end])
 
